ec9e72cd94518db66978bcf67a6946c4e8073cb2
Aminul-Islam7 <aminulfardin7@gmail.com>
2024-02-02 15:46:41 +0600

    feat: Add venue details to event page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,7 +29,7 @@
 	<link rel="stylesheet" href="{{asset('css/app.css')}}">
 	<link rel="stylesheet" href="{{asset('css/responsive.css')}}">
 
-		
+
 	<link rel="stylesheet" href="{{asset('css/all.css')}}">
 	<link rel="stylesheet" href="{{asset('css/sharp-solid.css')}}">
 	<link rel="stylesheet" href="{{asset('css/sharp-regular.css')}}">
@@ -42,24 +42,58 @@
 <body>
 	{{-- <div id="particles"></div> --}}
 	<button type="button" class="darkmode-btn" id="dark-mode-toggle">
-				<i class="color-theme-icon fa-solid fa-lightbulb"></i>
+		<i class="color-theme-icon fa-solid fa-lightbulb"></i>
 	</button>
-	
-  <section id="home">
-    <div class="container">
-      <img class="logo" src="{{asset('images/logo.png')}}" alt="Tumio Parbe">
-      <h1 class="title">
-        Annual Awards Ceremony
-        <br>
-        & Cultural Function 2024
-      </h1>
-			
-			{{ $slot }}
-				
-    </div>
-  </section>
 
-  
+	<section id="home">
+		<div class="container">
+			<img class="logo" src="{{asset('images/logo.png')}}" alt="Tumio Parbe">
+			<h1 class="title">
+				Annual Awards Ceremony
+				<br>
+				& Cultural Function 2024
+			</h1>
+
+			<div class="event-details">
+				<p class="date">
+					<i class="fa-regular fa-calendar-alt"></i>
+					8 March, 2024
+				</p>
+				<p class="location">
+					<i class="fa-regular fa-map-marker-alt"></i>
+					<span>International Mother Language Institute, Segunbagicha, Dhaka-1000 <a href="#venue">(Show map)</a></span>
+				</p>
+			</div>
+
+			{{ $slot }}
+
+
+			<h2 class="title">
+				Venue
+			</h2>
+			<div class="event-details">
+				<p class="location">
+					<i class="fa-regular fa-map-marker-alt"></i>
+					<span>আন্তর্জাতিক মাতৃভাষা ইনস্টিটিউট, সেগুন বাগিচা, ঢাকা-১০০০</a></span>
+				</p>
+			</div>
+			<iframe id="venue" class="map"
+				src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5165.1701477601455!2d90.40449098790114!3d23.735721655263678!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8f25c6effb9%3A0xdb923d496bcb57f0!2zSW50ZXJuYXRpb25hbCBNb3RoZXIgTGFuZ3VhZ2UgSW5zdGl0dXRlIOClpCDgpobgpqjgp43gpqTgprDgp43gppzgpr7gpqTgpr_gppUg4Kau4Ka-4Kak4KeD4Kat4Ka-4Ka34Ka-IOCmh-CmqOCmuOCnjeCmn-Cmv-Cmn-Cmv-CmieCmnw!5e0!3m2!1sen!2sbd!4v1706836887167!5m2!1sen!2sbd"
+				height="300" style="border:0;" allowfullscreen="" loading="lazy"
+				referrerpolicy="no-referrer-when-downgrade"></iframe>
+		</div>
+		<footer>
+			<p>
+				Copyright {{ date('Y')}} &copy; <a href="https://tumioparbe.com">TUMIO PARBE</a>
+			</p>
+			<p>
+				Designed & Developed by Aminul Islam
+			</p>
+		</footer>
+
+	</section>
+
+
 	<script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
 	<script src="{{asset('js/particles.min.js')}}"></script>
 	<script src="{{asset('js/embed.min.js')}}"></script>
